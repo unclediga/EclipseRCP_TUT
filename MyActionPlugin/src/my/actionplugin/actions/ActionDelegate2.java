c63e654d0ed7511ab1ca6abfd9d80882b1904412
BorodinovI <igordin@yandex.ru>
2014-08-08 17:46:44 +0400

    Игралсяс командами.

---
+++ MyActionPlugin/src/my/actionplugin/actions/ActionDelegate2.java
@@ -0,0 +1,28 @@
+package my.actionplugin.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+public class ActionDelegate2 implements IViewActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IViewPart view) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyActionPlugin/src/my/actionplugin/actions/PopupDelegat.java
@@ -0,0 +1,9 @@
+package my.actionplugin.actions;
+
+public class PopupDelegat {
+
+	public PopupDelegat() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
