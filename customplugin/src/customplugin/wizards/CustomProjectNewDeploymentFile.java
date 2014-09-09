bea15f52d524d7cad87d98017491a49c5858bcd6
BorodinovI <igordin@yandex.ru>
2014-09-09 18:21:31 +0400

    Почти готовый визард для Deploy-файла. Недостатки: - создаётся файл, но с пустым содержимым. - Editor после создания файла автоматически не открывается. А если бы и попытался, то открылся бы какой-нить дефолтный, т.к. содержимое файла пусто.

--- customplugin/src/customplugin/wizards/CustomProjectNewDeploymentFile.java
+++ customplugin/src/customplugin/wizards/CustomProjectNewDeploymentFile.java
@@ -1,28 +1,46 @@
 package customplugin.wizards;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+
+import customplugin.Activator;
 
 public class CustomProjectNewDeploymentFile extends Wizard implements
 		INewWizard {
 
+	private IWorkbench _workbench;
+	private IStructuredSelection _selection;
+	private WizardNewFileCreationPage _page;
+
 	public CustomProjectNewDeploymentFile() {
 		setWindowTitle("New Deployment File");
 	}
-	
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		// TODO Auto-generated method stub
+		_workbench = workbench;
+		_selection = selection;
+
+		_page = new WizardDeploymentNewFileCreationPage(_selection);
+		addPage(_page);
 
 	}
 
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+
+		boolean result = false;
+		IFile file = _page.createNewFile();
+		result = file != null;
+		return result;
 	}
 
 }
