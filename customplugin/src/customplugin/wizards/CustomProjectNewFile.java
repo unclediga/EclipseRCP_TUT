4b3a60db7d854acfdc2e419c6bcafa59210c85be
BorodinovI <igordin@yandex.ru>
2014-09-10 11:33:58 +0400

    Рефакторинг классов-Визардов

---
+++ customplugin/src/customplugin/wizards/CustomProjectNewFile.java
@@ -0,0 +1,47 @@
+package customplugin.wizards;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+import org.eclipse.ui.ide.IDE;
+
+public abstract class CustomProjectNewFile extends Wizard  implements INewWizard{
+
+	private IWorkbench _workbench;
+	protected IStructuredSelection _selection;
+	protected WizardNewFileCreationPage _pageOne;
+
+	public CustomProjectNewFile(String wizardName) {
+		setWindowTitle(wizardName);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		_workbench = workbench;
+		_selection = selection;
+	}
+
+	@Override
+	public boolean performFinish() {
+		boolean result = false;
+	
+		IFile file = _pageOne.createNewFile();
+		result = file != null;
+	
+		if (result) {
+			try {
+				IDE.openEditor(_workbench.getActiveWorkbenchWindow()
+						.getActivePage(), file);
+			} catch (PartInitException e) {
+				e.printStackTrace();
+			}
+		}
+	
+		return result;
+	}
+
+}
--- customplugin/src/customplugin/wizards/CustomProjectNewSchemaFile.java
+++ customplugin/src/customplugin/wizards/CustomProjectNewSchemaFile.java
@@ -1,48 +1,11 @@
 package customplugin.wizards;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
-import org.eclipse.ui.ide.IDE;
 
-public class CustomProjectNewSchemaFile extends Wizard implements INewWizard {
+public class CustomProjectNewSchemaFile extends CustomProjectNewFile {
 
 	private static final String WIZARD_NAME = "New Schema File";
-	private IWorkbench _workbench;
-	private IStructuredSelection _selection;
-	private WizardNewFileCreationPage _pageOne;
-
 	public CustomProjectNewSchemaFile() {
-		setWindowTitle(WIZARD_NAME);
-	}
-
-	@Override
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		_workbench = workbench;
-		_selection = selection;
-	}
-
-	@Override
-	public boolean performFinish() {
-		boolean result = false;
-
-		IFile file = _pageOne.createNewFile();
-		result = file != null;
-
-		if (result) {
-			try {
-				IDE.openEditor(_workbench.getActiveWorkbenchWindow()
-						.getActivePage(), file);
-			} catch (PartInitException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
+		super(WIZARD_NAME);
 	}
 
 	@Override
